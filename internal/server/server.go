2562039cd1f99400d153a092d05ac517f7ef9d13
Makov Andrew <machok06@yandex.ru>
2025-04-26 00:11:09 +0300

    implementation of the final task

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,47 @@
+// Пакет server реализует функционал для создания http-сервера.
+
 package server
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/handlers"
+)
+
+type Server struct {
+	logger *log.Logger
+	server *http.Server
+}
+
+func NewServer(logger *log.Logger) *Server {
+	router := http.NewServeMux()
+
+	router.HandleFunc("/", handlers.RootHandler)
+	router.HandleFunc("/upload", handlers.UploadHandler)
+
+	httpServer := &http.Server{
+		Addr:         ":8080",
+		Handler:      router,
+		ErrorLog:     logger,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+
+	return &Server{
+		logger: logger,
+		server: httpServer,
+	}
+}
+
+func (s *Server) Start() error {
+	s.logger.Printf("Starting server on %s", s.server.Addr)
+	return s.server.ListenAndServe()
+}
+
+func (s *Server) Shutdown() error {
+	s.logger.Println("Shutting down server")
+	return s.server.Close()
+}
